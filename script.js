76f0bfdcc339f1bd0774e6b6ae27a270020bbd11
bosseschum <bosse.schum@gmail.com>
2026-01-06 16:01:05 +0100

    Separate script file with game logic

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("Hello World!");
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+
+  if (choice === 0) {
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let choice = prompt("Rock, Paper, Scissors, Shoot!");
+  choice = choice.toLowerCase();
+
+  return choice;
+}
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+      return "Draw!";
+    }
+
+    // Human choice loses
+    if (
+      (humanChoice === "rock" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "rock")
+    ) {
+      computerScore++;
+      return `You lose! 
+        Your score: ${humanScore} | Computer score: ${computerScore}`;
+      // Human choice wins
+    } else {
+      humanScore++;
+      return `You win! 
+        Your score: ${humanScore} | Computer score: ${computerScore}`;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(humanSelection, computerSelection));
+  }
+}
+
+playGame();
